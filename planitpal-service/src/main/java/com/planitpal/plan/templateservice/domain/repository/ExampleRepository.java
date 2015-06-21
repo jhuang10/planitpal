3ed26598b8c797cb5c9e8d463ba49592f582af6b
Johnny Huang <johnnyhuang0@gmail.com>
2015-06-21 16:01:24 -0700

    Starting over

--- planitpal-service/src/main/java/com/planitpal/plan/templateservice/domain/repository/ExampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.planitpal.plan.templateservice.domain.repository;
-
-import com.planitpal.plan.templateservice.domain.Example;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExampleRepository extends CrudRepository<Example, String> {
-}
--- planitpal-service/src/main/java/com/planitpal/plan/templateservice/exceptions/ExampleNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.planitpal.plan.templateservice.exceptions;
-
-public class ExampleNotFoundException extends Exception {
-
-    public ExampleNotFoundException(String s) {
-        super(s);
-    }
-}
