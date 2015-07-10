6cefbd168ac9ecf388ddeedd9b368b11bb53cb15
Johnny Huang <Johnny_Huang@gap.com>
2015-07-10 15:39:24 -0700

    Add logging file appender to project

--- planitpal-service/src/main/java/com/planitpal/service/controller/IndexController.java
+++ planitpal-service/src/main/java/com/planitpal/service/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.planitpal.service.controller;
 
 import com.planitpal.service.domain.repository.PlanitpalRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import static com.planitpal.service.domain.Plan.plan;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     @Autowired
     private PlanitpalRepository planitpalRepository;
-
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String showIndex() {
+        log.debug("hello my name is Johnny");
+        log.info("Hello world project");
         planitpalRepository.save(plan("johnny"));
         return "Hello world 2";
     }
